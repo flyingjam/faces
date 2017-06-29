d4f06971f5254ba2d2648e9426346cb0ce660818
Starr Yang <starrtyang@gmail.com>
2017-06-29 07:44:53 -0700

    Added option to only process images and not download them

--- create_dataset.py
+++ create_dataset.py
@@ -15,20 +15,31 @@ def download_dataset(tag, n_images, download_url, out_dir, site_name, is_out_dir
     if is_out_dir:
         out_dir = os.path.join(out_dir, site_name)
 
-    print(out_dir)
+    convert_to_faces(tag, in_directory=in_dir, out_directory=out_dir)
+
+def only_process(tag, out_dir, site_name, is_out_dir):
+    in_dir = os.path.join("data/raw/", site_name)
+    out_dir = os.path.join("data", out_dir)
+    if is_out_dir:
+        out_dir = os.path.join(out_dir, site_name)
+
     convert_to_faces(tag, in_directory=in_dir, out_directory=out_dir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Downloads anime pictures, then crops faces into a seperate directory.")
     parser.add_argument('-s', '--site', action='store', default = "danbooru", help="Name of the site to download from, ex. Danbooru")
     parser.add_argument('-n', '--number', action='store', type=int, default="1000", help="Number of images to download")
-    parser.add_argument("tag", action='store', help="Tag to search for")
+    parser.add_argument('tag', action='store', help="Tag to search for")
     parser.add_argument('-d', '--dir', action='store', default="faces", help="Directory that processed faces are stored")
     parser.add_argument('-sd', '--site_dir', action='store_true', help="Appends site name to the output dictory")
+    parser.add_argument('-po', '--process_only', actions='store_true', help="Only process images into faces")
     args = parser.parse_args()
 
-    if args.site not in site_url_dict.keys():
-        raise RuntimeError("Site not supported!")
+    if args.process_only:
+        only_process(args.tag, args.dir, args.site, args.site_dir)
+    else:
+        if args.site not in site_url_dict.keys():
+            raise RuntimeError("Site not supported!")
 
-    site_url = site_url_dict[args.site]
-    download_dataset(args.tag, args.number, site_url, args.dir, args.site, args.site_dir)
+        site_url = site_url_dict[args.site]
+        download_dataset(args.tag, args.number, site_url, args.dir, args.site, args.site_dir)
