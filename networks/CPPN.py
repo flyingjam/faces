40a0b3d755ddacb3deb3c4a51a99e8458e9a4ddb
a person <tehflyingjam@gmail.com>
2017-07-24 15:41:30 -0400

    Added coordinate function which generates a tensor of coordinates of shape [batch_size * area, 3]

--- networks/CPPN.py
+++ networks/CPPN.py
@@ -3,3 +3,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from torch.autograd import Variable
+
+def coordinates(batch, x_dim = 32, y_dim = 32, scale = 1.0):
+    '''
+    calculates and returns a vector of x and y coordintes, and corresponding radius from the centre of image.
+    '''
+    n_points = x_dim * y_dim
+    x_range = scale*(np.arange(x_dim)-(x_dim-1)/2.0)/(x_dim-1)/0.5
+    y_range = scale*(np.arange(y_dim)-(y_dim-1)/2.0)/(y_dim-1)/0.5
+    x_mat = np.matmul(np.ones((y_dim, 1)), x_range.reshape((1, x_dim)))
+    y_mat = np.matmul(y_range.reshape((y_dim, 1)), np.ones((1, x_dim)))
+    r_mat = np.sqrt(x_mat*x_mat + y_mat*y_mat)
+    x_mat = np.tile(x_mat.flatten(), batch).reshape(batch, n_points, 1).astype(np.float32)
+    y_mat = np.tile(y_mat.flatten(), batch).reshape(batch, n_points, 1).astype(np.float32)
+    r_mat = np.tile(r_mat.flatten(), batch).reshape(batch, n_points, 1).astype(np.float32)
+    return np.concatenate((x_mat, y_mat, r_mat), axis=2)
