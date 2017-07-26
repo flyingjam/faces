e54e1cd63bb597ae5867b3475065b850e66ee632
a person <tehflyingjam@gmail.com>
2017-07-25 21:29:32 -0400

    CPU supported, but cudnn error

--- networks/CPPN.py
+++ networks/CPPN.py
@@ -1,7 +1,12 @@
+import time
+import os
+from networks import Network, Result, Encoder
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from torch.utils.data import DataLoader
 from torch.autograd import Variable
 
 def coordinates(batch, x_dim = 32, y_dim = 32, scale = 1.0):
@@ -31,7 +36,7 @@ def combine(coord, z):
     out = torch.from_numpy(out).float()
     return out
 
-def create_latent(latent, batch, x_dim, y_dim, z_dim):
+def create_latent(latent, batch, x_dim, y_dim):
     #latent vectors from encoder of shape [batch, z_dim]
     z = np.repeat(latent, x_dim * y_dim, axis=1)
     return z.reshape(1, batch * x_dim * y_dim, -1)
@@ -69,5 +74,125 @@ class CPPN(nn.Module):
         out = X.view(-1, self.z_dim + 3, 1)
         out = self.net(out)
         return out
+
+default_params = {
+    'z_channel': 16,
+    'z_dim': 256,
+    'encoder dropout': 0.5,
+    'decoder dropout': 0.3,
+    'batch size': 200,
+    'epochs': 20,
+    'learning rate': 0.01
+}
+
+class CPPNNetwork(Network):
+
+    def __init__(self, dataset, hyper_params = {}, cuda = False, width = 32, height = 32):
+
+        self.width = 32
+        self.height = 32
+
+        RGB = 3
+
+        self.use_cuda = cuda
+        self.params = default_params
+        self.params.update(hyper_params)
+        self.n_channel = RGB
+
+        self.save_path = "saved_networks/CPPN"
+
+        self.dataloader = DataLoader(dataset = dataset,
+                                     shuffle = True,
+                                     batch_size = self.params['batch size'],
+                                     drop_last = True)
+
+        self.encoder = Encoder(width,
+                               RGB,
+                               self.params['z_channel'],
+                               self.params['z_dim'],
+                               self.params['encoder dropout'],
+                               self.use_cuda)
+        self.decoder = CPPN(self.params['z_dim'],
+                            RGB,
+                            self.params['z_channel'],
+                            self.params['decoder dropout'])
+
+        parameters = list(self.encoder.parameters()) + list(self.decoder.parameters())
+        self.optimizer = torch.optim.Adam(parameters, self.params['learning rate'])
+
+        if self.use_cuda:
+            self.encoder.cuda()
+            self.decoder.cuda()
+
+    def train_epoch(self, epoch = 0):
+        TINY = 1e-15
+        before_time = time.clock()
+
+        self.encoder.train()
+        self.decoder.train()
+
+        for img, label in self.dataloader:
+            self.optimizer.zero_grad()
+
+            X = Variable(img)
+            if self.use_cuda:
+                X = X.cuda()
+
+            z_mu, z_var = self.encoder(X)
+            z = self.encoder.sample(self.params['batch size'], z_mu, z_var)
+
+            if self.use_cuda:
+                z = z.cpu()
+            z = z.data.numpy()
+
+            latent = create_latent(z, self.params['batch size'], self.width, self.height)
+            coords = coordinates(self.params['batch size'], self.width, self.height)
+            inp = Variable(combine(coords, latent))
+
+            if self.use_cuda:
+                inp = inp.cuda()
+
+            X_reconstructed = self.decoder(inp)#.view(self.params['batch size'], self.n_channel, self.width, self.height)
+            reconstruction_loss = F.binary_cross_entropy(X_reconstructed + TINY, X + TINY, size_average = False)
+            KL_loss = z_mu.pow(2).add_(z_var.exp()).mul_(-1).add_(1).add_(z_var)
+            KL_loss = torch.sum(KL_loss).mul_(-0.5)
+
+            total_loss = reconstruction_loss + KL_loss
+            total_loss.backward()
+            self.optimizer.step()
+
+        duration = time.clock() - before_time
+
+        def loss_reporting(loss):
+            return "{}".format(loss.data[0])
+
+        report = Result(duration, total_loss, epoch, loss_reporting)
+        return report
+
+
+
+    def save(self, name):
+        pass
+
+    def load(self, name):
+        pass
+
+    def sample(self, *img):
+        self.encoder.eval()
+        self.decoder.eval()
+
+        #todo image support
+
+        z = torch.FloatTensor(self.params['batch size'], self.params['z_dim']).normal_()
+        z = z.numpy()
+        latent = create_latent(z, self.params['batch size'], self.width, self.height)
+        coords = coordinates(self.params['batch size'], self.width, self.height)
+        inp = Variable(combine(coords, latent))
+        if self.use_cuda:
+            inp = inp.cuda()
+
+        result = self.decoder(inp)
+        return result.data.cpu()
+
 
 
